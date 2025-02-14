4aaea83453e23ffccfaa56923068f1fe33035c28
Efrén Fuentes <developer@efrenfuentes.net>
2025-02-13 23:04:58 -0400

    prepare package to publish in hex.pm

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Text2Html.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Text2Html",
+      source_url: "https://github.com/efrenfuentes/text2html",
+      homepage_url: "https://github.com/efrenfuentes/text2html",
+      docs: [
+        main: "Text2Html"
+      ]
     ]
   end
 
@@ -23,7 +31,21 @@ defmodule Text2Html.MixProject do
     [
       {:phoenix_html, "~> 2.11 or ~> 3.0 or ~> 4.0"},
       {:phoenix_html_helpers, "~> 1.0"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Text2Html transform text into HTML.."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "text2html",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/efrenfuentes/text2html"}
     ]
   end
 end
